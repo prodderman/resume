88edfcf9d0a6eafd9557b62a34fc2dadd35dab2b
prodderman <rainbowprodder@gmail.com>
2018-06-02 23:59:11 +0700

    updates

--- src/pages/resume-page/resume-page.js
+++ src/pages/resume-page/resume-page.js
@@ -1,12 +1,10 @@
 import 'normalize.css';
 import '../../global/global.styl';
 
-const cache = {};
-function importAll (r) {
-  r.keys().forEach(key => cache[key] = r(key));
-}  
-importAll(require.context('../../components/', true, /^\.\/.*\.jsx$/));
-importAll(require.context('../../components/', true, /^\.\/.*\.styl$/));
+function importAllStyles (context) {
+  context.keys().forEach(key => context(key));
+}
+importAllStyles(require.context('../../components/', true, /^\.\/.*\.styl$/));
 
 if (module.hot) {
     module.hot.accept();
